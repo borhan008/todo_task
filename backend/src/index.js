1ae7c2d836dbbf3bbe59b67fd36cb50d3c4c076d
knowinlyunknown <sultanulborhan@gmail.com>
2025-02-25 15:10:05 +0600

    initializing server and all related things

---
+++ backend/src/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const passport = require("passport");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 365,
+    },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
