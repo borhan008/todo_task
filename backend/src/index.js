94591089dba2ae5d9573a09dc722616c054a24c1
knowinlyunknown <sultanulborhan@gmail.com>
2025-02-28 11:21:27 +0600

    Vercel cors issue

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["https://todo-task-7i4d.vercel.app/", "http://localhost:3000"],
+    origin: ["https://todo-task-7i4d.vercel.app", "http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
